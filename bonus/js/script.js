54a1fbd5e83dae2effb409d6ba7390329f69a446
sourceCodeISA <isabel.gogov1@libero.it>
2020-11-16 17:42:43 +0100

    aggiunto immagine dinamicamente

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -1,1 +1,9 @@
 // Bonus: Aggiungere un’immagine presa anch’essa da un data
+
+var root = new Vue({
+  el: "#root",
+  data: {
+    // image: 'https://d2jq2hx2dbkw6t.cloudfront.net/231/vue-js-2.1.0.jpg'
+    image: src="./img/vue-js.jpg"
+  }
+});
