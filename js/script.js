3996834025b1a76eefeafaab70a97c17709b9068
sourceCodeISA <isabel.gogov1@libero.it>
2020-11-16 16:41:16 +0100

    esercizio base finito

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data
+
+var root = new Vue({
+  el: "#root",
+  data: {
+    message: 'Hello World!!!!!'
+  }
+});
--- script.js
+++
@@ -1,1 +0,0 @@
-// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data
