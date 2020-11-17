234f58b9f15eafb0c7594026ced27e7611bcccc7
sourceCodeISA <isabel.gogov1@libero.it>
2020-11-17 10:06:58 +0100

    correzione ex bonus

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -4,6 +4,6 @@ var root = new Vue({
   el: "#root",
   data: {
     // image: 'https://d2jq2hx2dbkw6t.cloudfront.net/231/vue-js-2.1.0.jpg'
-    image: src="./img/vue-js.jpg"
+    image: "img/vue-js.jpg"
   }
 });
